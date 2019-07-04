1370541698d1598c885252f5ab91af260cab5eb4
monmonplaza <monmon.plaza@gmail.com>
2019-07-04 16:03:48 +0800

    fix feature

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,42 @@
-var toggleMenu = document.querySelector('.toggle-menu');
-var sidenav = document.querySelector('.side-nav');
-var close = document.querySelector('.close');
-var main = document.querySelector('main');
+//SIDEMENU
+var toggleMenu = document.querySelector(".toggle-menu");
+var sidenav = document.querySelector(".side-nav");
+var close = document.querySelector(".close");
+var main = document.querySelector("main");
+//Modal
+var loginModal = document.querySelector("#login-modal");
+var closeLogin = document.querySelector("#close-login");
+var openLogin = document.querySelector("#open-login");
 
-toggleMenu.addEventListener('click', function () {
+//FUNCTION
 
-    main.classList.add('move-right');
-    sidenav.classList.add('open');
+toggleMenu.addEventListener("click", function() {
+  main.classList.add("move-right");
+  sidenav.classList.add("open");
 });
 
-close.addEventListener('click', function () {
-
-    main.classList.remove('move-right');
-    sidenav.classList.remove('open');
+close.addEventListener("click", function() {
+  main.classList.remove("move-right");
+  sidenav.classList.remove("open");
 });
+
+eventListener();
+function eventListener() {
+  closeLogin.addEventListener("click", closeModal);
+  openLogin.addEventListener("click", openModal);
+  window.addEventListener("click", outsideClickModal);
+}
+
+function openModal() {
+  loginModal.style.display = "block";
+}
+
+function closeModal() {
+  loginModal.style.display = "none";
+}
+
+function outsideClickModal(e) {
+  if (e.target == loginModal) {
+    loginModal.style.display = "";
+  }
+}
