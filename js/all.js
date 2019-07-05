9302d8f54a5b27b92eab4801cded68546380c1a9
monmonplaza <monmon.plaza@gmail.com>
2019-07-05 14:17:42 +0800

    Updates

--- js/all.js
+++ js/all.js
@@ -1,1 +1,42 @@
-var toggleMenu=document.querySelector(".toggle-menu"),sidenav=document.querySelector(".side-nav"),close=document.querySelector(".close"),main=document.querySelector("main");toggleMenu.addEventListener("click",function(){main.classList.add("move-right"),sidenav.classList.add("open")}),close.addEventListener("click",function(){main.classList.remove("move-right"),sidenav.classList.remove("open")});
+//SIDEMENU
+var toggleMenu = document.querySelector(".toggle-menu");
+var sidenav = document.querySelector(".side-nav");
+var close = document.querySelector(".close");
+var main = document.querySelector("main");
+//Modal
+var loginModal = document.querySelector("#login-modal");
+var closeLogin = document.querySelector("#close-login");
+var openLogin = document.querySelector("#open-login");
+
+//FUNCTION
+
+toggleMenu.addEventListener("click", function() {
+  main.classList.add("move-right");
+  sidenav.classList.add("open");
+});
+
+close.addEventListener("click", function() {
+  main.classList.remove("move-right");
+  sidenav.classList.remove("open");
+});
+
+eventListener();
+function eventListener() {
+  closeLogin.addEventListener("click", closeModal);
+  openLogin.addEventListener("click", openModal);
+  window.addEventListener("click", outsideClickModal);
+}
+
+function openModal() {
+  loginModal.style.display = "block";
+}
+
+function closeModal() {
+  loginModal.style.display = "none";
+}
+
+function outsideClickModal(e) {
+  if (e.target == loginModal) {
+    loginModal.style.display = "";
+  }
+}
